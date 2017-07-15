5f02575c7571ab7a58d386915edf62fba13ed08d
Hristo Hristov <koredalin@gmail.com>
2017-07-15 17:58:13 +0300

    Login init

--- src/AdminBundle/Controller/DefaultController.php
+++ src/AdminBundle/Controller/DefaultController.php
@@ -12,6 +12,13 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('AdminBundle:Default:index.html.twig');
+        if (!$this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
+            throw $this->createAccessDeniedException();
+        }
+
+        // the above is a shortcut for this
+        $user = $this->get('security.token_storage')->getToken()->getUser();
+        
+        return $this->render('AdminBundle:Default:index.html.twig', array('user' => $user));
     }
 }
